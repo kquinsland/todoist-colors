0db4421b1d45136ad62c8f23aeeec53cf6a03cf8
Karl Q <kquinsland@users.noreply.github.com>
2020-06-18 20:59:26 -0700

    Update __init__.py
    globals -> map

--- todoist_colors/__init__.py
+++ todoist_colors/__init__.py
@@ -18,3 +18,27 @@ MONA_LISA = 46
 GRAY = 47
 LIGHT_GRAY = 48
 SORREL_BROWN = 49
+
+# Expressed as a map
+colors = {
+    'HIBISCUS': 30,
+    'RED': 31,
+    'ORANGE': 32,
+    'YELLOW': 33,
+    'EARLS_GREEN': 34,
+    'LIGHT_GREEN': 35,
+    'GREEN': 36,
+    'DOWNY': 37,
+    'EASTERN_BLUE': 38,
+    'DODGER_BLUE': 39,
+    'CORNFLOWER': 40,
+    'BLUE': 41,
+    'VIOLET': 42,
+    'PURPLE': 43,
+    'LIGHT_ROSE': 44,
+    'ROSE': 45,
+    'MONA_LISA': 46,
+    'GRAY': 47,
+    'LIGHT_GRAY': 48,
+    'SORREL_BROWN': 49
+}
